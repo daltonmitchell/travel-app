2773e81ac894ca4c345f8d1cf917b16739f6821d
daltonmitchell <dzack34@gmail.com>
2022-08-02 16:37:46 -0700

    MERN-Stack Infrastructure

---
+++ src/utilities/users-service.js
@@ -0,0 +1,39 @@
+import { token } from 'morgan';
+import * as usersAPI from './users-api';
+
+export async function signUp(userData){
+    const token = await usersAPI.signUp(userData);
+    localStorage.setItem('token', token);
+    return token;
+}
+
+export async function logIn(userData){
+    const token = await usersAPI.logIn(userData);
+    localStorage.setItem('token', token);
+    return getUser();
+}
+
+export function logOut(){
+    localStorage.removeItem('token');
+}
+
+export function getToken(){
+    const token = localStorage.getItem('token');
+    if(!token) return null;
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    if(payload.exp < Date.now() / 1000){
+        localStorage.removeItem('token');
+        return null;
+    }
+    return token;
+}
+
+export function checkToken(){
+    return usersAPI.checkToken()
+        .then(dateStr => new Date(dateStr));
+}
+
+export function getUser(){
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
